91e9d586bf7980c5dc51c6bd40ba734b4d5a6fda
muatasimqazi <muatasimqazi@gmail.com>
2016-11-03 23:32:10 -0700

    Added registration and home page file

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,30 @@
+<<<<<<< HEAD
  $('#textarea1').val('New Text');
   $('#textarea1').trigger('autoresize');
 
    $(document).ready(function() {
     Materialize.updateTextFields();
   });
+=======
+ // $('#textarea1').val('New Text');
+ //  $('#textarea1').trigger('autoresize');
+
+ $(document).ready(function() {
+     Materialize.updateTextFields();
+
+     $(".tabs li").click(function() {
+         $(this).toggleClass("active teal z-depth-1");
+         $(this).siblings().removeClass("active teal z-depth-1");
+
+     });
+
+     $(".tab").click(function() {
+         $(this).toggleClass("");
+         $(this).siblings().removeClass("white-text text-darken-2");
+
+     });
+
+
+
+ });
+>>>>>>> Added registration and home page file
