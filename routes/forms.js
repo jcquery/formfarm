ff57891855d8d6e485ac79c422e8f435577115fd
John Carney <jcarney000@gmail.com>
2016-11-04 00:23:02 -0700

    add get form list and post new form template routes

--- routes/forms.js
+++ routes/forms.js
@@ -31,4 +31,55 @@ router.get('/form/:id', (req, res, next) => {
     });
 });
 
+router.get('/forms', (req, res, next) => {
+  const userId = 1;
+
+  knex('users')
+    .select('forms.name', 'forms.id')
+    .where('users.id', userId)
+    .innerJoin('farms', 'farms.id', 'users.farm_id')
+    .innerJoin('forms', 'forms.farm_id', 'farms.id')
+    .then((forms) => {
+      res.send(forms);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/form', (req, res, next) => {
+  const { name, options } = req.body;
+  const userId = 1;
+  const newForm = {};
+
+  knex('users')
+    .select('farm_id')
+    .where('users.id', userId)
+    .first()
+    .then((farmId) => {
+      return knex('forms')
+        .insert({ name, farm_id: farmId.farm_id }, '*');
+    })
+    .then((form) => {
+      newForm.id = form[0].id;
+      newForm.name = form[0].name;
+      console.log(newForm);
+      return options.map((optId) => {
+        return knex('forms-options').insert({
+          form_id: newForm.id,
+          option_id: optId
+        });
+      });
+    })
+    .then((promises) => {
+      return Promise.all(promises);
+    })
+    .then(() => {
+      res.send(newForm);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 module.exports = router;
