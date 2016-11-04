a7a5158cd4100035cb906ff0fb776df12cffc27e
Ryan Rotz <ryanrotz@gmail.com>
2016-11-03 21:00:02 -0700

    seeded database with 4_options.js in the seeds folder

--- seeds/4_options.js
+++ seeds/4_options.js
@@ -4,11 +4,104 @@ exports.seed = function(knex) {
   
   return knex('options').del()
     .then(() => {
-      return knex('options').insert([{
-        id: 1,
-        name: 'Team Leader',
-        type: 'text'
-      }]);
+      return knex('options').insert([
+        {
+          id: 1,
+          name: 'Team Leader',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Location',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'What workers are present in am?',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Are there new tools or procedures that need to be talked about?',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'What are the hazards to be aware of?',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Personal protective equipment needed?',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'When lifting heavy objects, the large muscles of the leg instead of the smaller muscles of the back shall be used.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Inappropriate footwear or shoes with thin or badly worn soles shall not be worn.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Safe practices for operating any agricultural equipment, including procedures for cleaning, repairing, servicing and adjusting.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Electrical hazards',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Heat stress.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Ergonomic hazards, including proper lifting techniques and working on ladders or in a stooped posture for prolonged periods at one time.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Hazardous chemical exposures.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Guarding of belts and pulleys, gears and sprockets, and conveyor nip points. ',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Machine, machine parts, and prime movers guarding.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Lock-out/tag-out procedures.',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Materials handling. ',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Ergonomic hazards, including proper lifting techniques. ',
+          type: 'text'
+        },
+        {
+          id: 1,
+          name: 'Noise',
+          type: 'text'
+        }
+
+      ]);
     })
     .then(() => {
       return knex.raw(
